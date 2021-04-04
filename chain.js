a5b3770147c714fa74126e41f02af00933b56366
bidyut kumar nath <75525355+bidyutkumarnath@users.noreply.github.com>
2021-04-04 03:17:53 -0700

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,17 @@
+class chain {
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.5,
+            length:15
+        }
+        this.chain = Constraint.create(options)
+        World.add(world,this.chain)
+
+    }
+    display(){
+        strokeWeight(2)
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+    }
+}
